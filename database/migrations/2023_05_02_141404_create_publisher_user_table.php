476dc119570fbbac4c28fabfcfda5773ab2dd156
Hasbiy Robbiy <99758182+has-bii@users.noreply.github.com>
2023-06-04 18:19:06 +0300

    Fixed pivot table

--- database/migrations/2023_05_02_141404_create_publisher_user_table.php
+++ database/migrations/2023_05_02_141404_create_publisher_user_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user_publisher', function (Blueprint $table) {
+        Schema::create('publisher_user', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('publisher_id')->unsigned();
             $table->bigInteger('user_id')->unsigned();
